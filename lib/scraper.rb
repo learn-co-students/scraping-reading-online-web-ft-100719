ef0e55a81aca598ed3589b483eb7147d778b9a2f
Jesse <jesse.lee.smith@gmail.com>
2019-10-21 21:23:59 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .text-4GLMvr .title-oE5vT4")
+
+courses.each {|course| puts course.text.strip}
